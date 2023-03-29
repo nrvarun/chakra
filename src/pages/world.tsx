9a1a11f4904f9628dfefe36e6e8823de48eac6e6
Varun <inr.varun89@gmail.com>
2023-03-29 17:16:46 +0530

    fucking bug fixed

--- src/pages/world.tsx
+++ src/pages/world.tsx
@@ -1,9 +1,9 @@
 // import { Inter } from "next/font/google";
 // const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
-import CustomHead from "../components/CustomHead";
-import Header from "../components/Header";
-import WorldModule from "../modules/WorldModule";
+import CustomHead from "@components/CustomHead";
+import Header from "@components/Header";
+import WorldModule from "modules/WorldModule";
 
 export default function World() {
   return (
