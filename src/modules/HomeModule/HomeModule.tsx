a3291ce967a030c617c474ea54a41730d40546c0
Varun <inr.varun89@gmail.com>
2023-03-29 16:17:15 +0530

    [fix] - Build errors fixed

--- src/modules/HomeModule/HomeModule.tsx
+++ src/modules/HomeModule/HomeModule.tsx
@@ -1,4 +1,3 @@
-import Footer from "@components/Footer";
 import Image from "next/image";
 import React from "react";
 import {
--- src/pages/world.tsx
+++ src/pages/world.tsx
@@ -3,7 +3,6 @@
 import Header from "@components/Header";
 import CustomHead from "@components/CustomHead";
 
-import Footer from "@components/Footer";
 import WorldModule from "@modules/WorldModule";
 
 // const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600"] });
