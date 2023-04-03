663ad5f1bfe9ab2bc15a113a6d89671c4f55bb04
Varun <inr.varun89@gmail.com>
2023-04-03 20:02:11 +0530

    [feat] - SEO Changes

--- src/Components/CustomHead/CustomHead.tsx
+++ src/Components/CustomHead/CustomHead.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import Head from "next/head";
 
-type Props = {};
+type Props = {
+  bannerImg?: string;
+};
 
-const CustomHead = (props: Props) => {
+const CustomHead = ({ bannerImg = "/images/ogg/banner.png" }: Props) => {
   return (
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -12,9 +15,26 @@ const CustomHead = (props: Props) => {
         rel="stylesheet"
       />
       <title>Chakra</title>
-      <meta name="description" content="The world and lore" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Chakra" />
+
+      <meta
+        name="twitter:description"
+        content="Chakra is a dynamic, ever expanding IP anchored in eastern mythologies that mimics natural evolution of human mythos and culture. Chakra seeks to speedrun millennia-long mythmaking by bringing it to life within a high-fidelity sandbox by leveraging mix media (Gaming, Cinematics & Comics) & web3 tech- [DeFi & NFTs]."
+      />
+      <meta
+        name="description"
+        content="Chakra is a dynamic, ever expanding IP anchored in eastern mythologies that mimics natural evolution of human mythos and culture. Chakra seeks to speedrun millennia-long mythmaking by bringing it to life within a high-fidelity sandbox by leveraging mix media (Gaming, Cinematics & Comics) & web3 tech- [DeFi & NFTs]."
+      />
+      <meta property="og:title" content="chakra - We are coming to Metaverse" />
+      <meta
+        property="og:description"
+        content="Chakra is a dynamic, ever expanding IP anchored in eastern mythologies that mimics natural evolution of human mythos and culture. Chakra seeks to speedrun millennia-long mythmaking by bringing it to life within a high-fidelity sandbox by leveraging mix media (Gaming, Cinematics & Comics) & web3 tech- [DeFi & NFTs]."
+      />
+      <meta property="og:image" content={bannerImg} />
+      <meta name="twitter:image:src" content={bannerImg} />
+      <meta name="twitter:image" content={bannerImg} />
     </Head>
   );
 };
