3fc166c2de953a1d96bee7c422fc87d821200f92
Varun <inr.varun89@gmail.com>
2023-06-05 20:45:03 +0530

    work in progress

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,6 +3,9 @@ import { StyledHeader, StyledHeaderLogoWrapper } from "./header.style";
 
 import SVG from "react-inlinesvg";
 import { useRouter } from "next/router";
+import { useWindowSize } from "react-use";
+import HeaderNav from "./HeaderNav";
+import MobileNav from "./MobileNav";
 
 type Props = {
   logo?: React.ReactNode;
@@ -10,76 +13,21 @@ type Props = {
   customClass?: string;
 };
 
-const NAV_LINKS = [
-  {
-    path: "/world",
-    title: "World",
-    disabled: false,
-    target: "",
-    tooltipContent: "",
-  },
-  {
-    path: "https://mirror.xyz/worldofchakra.eth/03pjbCCXlDpaBAFN6srpLu-XDlscyEqCtMGWYKxqVBY",
-    title: "Blog",
-    disabled: false,
-    target: "_blank",
-    tooltipContent: "🚧",
-  },
-  {
-    path: "/whitepaper",
-    title: "whitepaper",
-    disabled: false,
-    target: "",
-    tooltipContent: "",
-  },
-  // {
-  //   path: "https://continuous-rover-864.notion.site/Litepaper-85bcde5f6c18473a8a87f4c53bb4c88c",
-  //   title: "Docs",
-  //   disabled: false,
-  //   target: "_blank",
-  //   tooltipContent: "",
-  // },
-  {
-    path: "/connect",
-    title: "Play",
-    disabled: true,
-    target: "",
-    tooltipContent: "🚧",
-  },
-];
-
 const Header = ({ customClass = "" }: Props) => {
-  const router = useRouter();
-  const currentRoute = router.pathname;
+  const { width } = useWindowSize();
 
   return (
     <StyledHeader className={customClass}>
       <div className="grid grid-cols-2">
         <StyledHeaderLogoWrapper>
           <Link href={"/"} className="inline-block after:opacity-0">
-            <SVG src="/icons/logo.svg" />
+            <SVG
+              src={width > 1200 ? "/icons/mobile-logo.svg" : "/icons/logo.svg"}
+            />
           </Link>
+          <SVG src="/icons/hamburger.svg" />
         </StyledHeaderLogoWrapper>
-        <div className="relative flex">
-          <ul className="flex items-start ml-auto">
-            {NAV_LINKS.map((link) => (
-              <li className="px-1 lg:px-5 py-2 mr-1" key={link.path}>
-                <Link
-                  target={link.target}
-                  href={link.disabled ? "/" : link.path}
-                  className={`${
-                    currentRoute.includes(link.path) ? "active" : "in-active"
-                  } ${link.disabled ? "disabled" : ""}`}
-                >
-                  <p className="text-xs md:text-sm lg:text-base font-inter capitalize text-white">
-                    {link.title}
-                  </p>
-                  {link.disabled && <span>{link.tooltipContent}</span>}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        {width > 1200 ? <HeaderNav /> : <MobileNav />}
       </div>
     </StyledHeader>
   );
